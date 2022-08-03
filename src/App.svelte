deb16dc9ae2bf821d89995a0f463c0b5d6f5ee14
ciberedgar <edgar.rodriguez.antillon@uabc.edu.mx>
2022-08-02 17:23:28 -0700

    Update 0.1B

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,27 @@
 <script>
 	export let name;
-	import Mastiff from './Components/Masthead.svelte';
+	import { fade, fly } from 'svelte/transition';
+	import Navbar from './Components/Navbar.svelte';
 	import Footer from './Components/Footer.svelte';
 import Masthead from './Components/Masthead.svelte';
-</script>
 
+let windowY;
+let navWidth;
+</script>
+<svelte:window bind:scrollY={windowY}/>
 <main>
 	<head>
 		<meta charset="UTF-8">
 	</head>
+		{#if windowY >= navWidth}
+		<div in:fade="{{ y: 200, duration: 2000 }}" out:fade>
+		<Navbar />
+		</div>
+		{/if}
 	<div class="pb-1"><Masthead/></div>
 
-	<div class="min-h-screen flex flex-col items-center justify-center overflow-hidden ">
-		hola
+	<div bind:clientHeight={navWidth} class="min-h-screen flex flex-col items-center justify-center overflow-hidden ">
+		{windowY}
 	</div>
 	
 	<footer class="pt-6 text-center"><Footer/></footer>
